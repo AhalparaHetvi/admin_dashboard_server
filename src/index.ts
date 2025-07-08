f19e01d50f192a649f314a9b6588d37d72434844
Anup <anup.maurya484@gmail.com>
2025-07-08 14:32:36 +0530

    new-v

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,15 @@
 import express from 'express';
-import cors from 'cors'; // ✅ import cors
+import cors from 'cors';
 import dotenv from 'dotenv';
-import path from 'path';
-import connectDB from './config/db';
-import authRoutes from './routes/authRoutes';
-
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
+import connectDB from './config/db.ts';
+import authRoutes from './routes/authRoutes.ts';
 
 dotenv.config();
 connectDB();
 
 const app = express();
-
-// ✅ Enable CORS for all origins (dev only)
 app.use(cors());
-
-// or safer option (only allow your frontend)
-// app.use(cors({
-//   origin: 'http://localhost:3000',
-//   credentials: true
-// }));
-
-// app.use(cors({
-//   origin: '*', // Allow all origins
-// }));
-
 app.use(express.json());
-
-// Load Swagger YAML
-const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
-
-// Setup Swagger endpoint
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Auth routes
 app.use('/api/auth', authRoutes);
